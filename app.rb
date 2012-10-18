9019660a4916474ca42a28bb75639ce7537bed3a
Tsukasa Obara <obara@saucer.jp>
2012-10-19 02:35:38 +0900

    cache_controlの設定変更

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require 'sinatra'
 require 'rack/cache'
 require 'sinatra/reloader'
 
+configure do
+  set :static_cache_control => [:public, :max_age => 86400*3]
+end
+
 get '/' do
-  cache_control :public, max_age: 1800  # 30 mins.
+  cache_control :public, max_age: 86400*3  # 3 day
   erb :index
 end
